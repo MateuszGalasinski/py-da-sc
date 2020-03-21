1021b80b565ad7adf0895640dd8a6deb08c1abef
Mateusz <210183@edu.p.lodz.pl>
2020-03-21 19:12:59 +0100

    jup notebook

---
+++ runJupyter.ps1
@@ -0,0 +1,1 @@
+docker run --rm -it --user root -e NB_GID=100 -p 8888:8888 -v "$(pwd)/jupyter:/home/jovyan/host-note" jupyter/scipy-notebook:latest start-notebook.sh --NotebookApp.token='' --NotebookApp.allow_origin='*'
